04dbadb2ed2e4781b22aeac923ca54be708ca124
aliahmadcse <aliahmadcse@gmail.com>
2020-03-27 23:10:37 +0500

    Adds task card

--- public/index.php
+++ public/index.php
@@ -6,15 +6,42 @@
 
 
 <div class="page-wrapper">
+    <!-- search bar -->
     <div class="row">
         <div class="col-12 text-center mt-1">
             <div class="search">
-                <input type="text" class="searchTerm" placeholder="What are you looking for?">
-                <button type="submit" class="searchButton">
+                <input type="text" class="search-term" placeholder="Type to Search...">
+                <button type="submit" class="search-button">
                     <i class="fa fa-search"></i>
                 </button>
             </div>
         </div>
+    </div>
+
+    <div class="top-wrapper"></div>
+
+    <!-- --------------task card-------------- -->
+    <div class="row">
+        <div class="col-12">
+            <div class="card mb-2">
+                <div class="task-description">
+                    <label class="container ml-5">Write some code
+                        <input type="checkbox">
+                        <span class="checkmark m-1"></span>
+                    </label>
+                </div>
+                <div class="card-icon">
+                    <i class="mr-4 far fa-star"></i>
+                    <!-- <i class="mr-4 fas fa-star"></i> -->
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!-- ---------task card end------------ -->
+
+    <div class="row">
+        <div class="col-12"></div>
     </div>
 </div>
 
