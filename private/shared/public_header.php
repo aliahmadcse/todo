905225ea87edf74176ab330242dbe28b57f962f0
aliahmadcse <aliahmadcse@gmail.com>
2020-03-27 15:40:58 +0500

    Adds top navbar

--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -12,8 +12,37 @@
     <link rel="stylesheet" href="<?php echo url_for('/vendor/fontawesome/css/all.min.css'); ?>">
     <!-- custom css -->
     <link rel="stylesheet" href="<?php echo url_for('/stylesheets/public.css') ?>">
-    
+
 
 </head>
 
 <body>
+    <?php $is_login = false; ?>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="<?php echo url_for('/index.php'); ?>">Todo</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <?php if ($is_login) : ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Username</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sign out</a>
+                        </li>
+                    <?php else : ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sign In</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sign up</a>
+                        </li>
+                    <?php endif; ?>
+                </ul>
+            </div>
+        </nav>
+    </header>
