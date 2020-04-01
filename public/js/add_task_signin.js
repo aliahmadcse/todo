8fb524b17903666aa97a0523726ae376d3719852
aliahmadcse <aliahmadcse@gmail.com>
2020-04-02 01:05:20 +0500

    Binds the events to the newly created card

--- public/js/add_task_signin.js
+++ public/js/add_task_signin.js
@@ -1,3 +1,49 @@
+const markComplete = sentId => {
+  const idStr = sentId;
+  console.log(idStr);
+  // return;
+  const id = idStr.match(/\d+/)[0];
+  sendCompleteRequest(id);
+  const parentEle = $(`#${idStr}`).closest(".task-row");
+  parentEle.remove();
+};
+
+const sendCompleteRequest = id => {
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "ajax/complete_task.php");
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let result = xhr.responseText;
+      console.log(result);
+    }
+  };
+  xhr.send("id=" + id);
+};
+
+const markImportant = sentId => {
+  const idStr = sentId;
+  const id = idStr.match(/\d+/)[0];
+  sendImportantRequest(id);
+  const parentEle = $(`#${idStr}`).closest(".task-row");
+  parentEle.remove();
+};
+
+const sendImportantRequest = id => {
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "ajax/important_task.php");
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let result = xhr.responseText;
+      console.log(result);
+    }
+  };
+  xhr.send("id=" + id);
+};
+
 $(document).ready(function() {
   // const postURL = "todo/public/ajax/add_task.php";
   // sending request on ender key press event
@@ -33,17 +79,17 @@ $(document).ready(function() {
           <div class="task-description">
             <label class="container ml-5">${task}
               <input type="checkbox">
-                <span class="checkmark m-1 mark-complete" id="complete-${insertId}"></span>
+                <span class="checkmark m-1 mark-complete" id="complete-${insertId}" onclick="markComplete(this.id)"></span>
             </label>
           </div>
           <div class="card-icon">
-            <i class="mr-2 ml-4 far fa-star" id="imp-${insertId}"></i>
+            <i class="mr-2 ml-4 far fa-star" id="imp-${insertId}" onclick="markImportant(this.id)"></i>
           </div>
         </div>
       </div>
     </div>`;
 
-    return str.replace(/(\r\n|\n|\r)/gm,"");
+    return str.replace(/(\r\n|\n|\r)/gm, "");
   };
   //
 }); //end of jq doc ready event
